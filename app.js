2d4a45da44b3fa5f1ce8d20d5a45a4cf92dc59a6
Tomas David <tomasdavid@MacBook-Pro-de-Tomas.local>
2019-05-20 18:39:04 -0400

    installed web3 library and connected to the dummy contract under my new account

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const Web3 = require('web3')
+
+
+
+const rpcURL = 'http://localhost:8545'
+const web3 = new Web3(rpcURL)
+const address = '0x01a3476bdb5e0372b0f0e2e5c427831c5b9b464b' // Your account address goes here
+
+
 module.exports = function () {
-    return 'hello'
+    web3.eth.getBalance(address, (err, wei) => {
+        balance = web3.utils.fromWei(wei, 'ether')
+    })
 }
+
+
+
+
+
