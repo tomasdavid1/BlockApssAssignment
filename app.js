18c721c7b11b37a22b4ec41e279ed46007f12932
Tomas David <tomasdavid@MacBook-Pro-de-Tomas.local>
2019-05-21 14:24:57 -0400

    succesfully connected to the deployed smart contract and retrieved some information (gas estimate of transactions)

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
 const Web3 = require('web3')
 
 
+const rpcURL = '127.0.0.1:8545'
+const address = '0x0aa8a99b2bcfd726c5b7b9cf69815fc5ecfcbe11' // Your account address goes here
+const web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"))
 
-const rpcURL = 'http://localhost:8545'
-const web3 = new Web3(rpcURL)
-const address = '0x01a3476bdb5e0372b0f0e2e5c427831c5b9b464b' // Your account address goes here
 
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "x",
+                "type": "uint256"
+            }
+        ],
+        "name": "testSet",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "testGet",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
 
-module.exports = function () {
-    web3.eth.getBalance(address, (err, wei) => {
-        balance = web3.utils.fromWei(wei, 'ether')
-    })
+const Contract = web3.eth.contract(abi);
+
+var Test = Contract.at(address)
+
+x = Test.testGet.estimateGas()
+
+/*contract.methods.totalSupply().call((err, result) => { console.log(result) })
+contract.methods.name().call((err, result) => { console.log(result) })
+contract.methods.symbol().call((err, result) => { console.log(result) })
+const balance = contract.methods.balanceOf('0xd26114cd6EE289AccF82350c8d8487fedB8A0C07').call((err, result) => {  })
+*/
+
+module.exports = () => {
+    return x
+
 }
 
 
--- test/AppTest.js
+++ test/AppTest.js
@@ -2,7 +2,7 @@ const assert =  require('chai').assert
 const app =  require('../app.js')
 
 describe('App', function () {
-    it('hello', function () {
-        assert.equal(app(), 'hello')
+    it('gas estimates', function () {
+        assert.equal(app(), 21272)
     })
 })
